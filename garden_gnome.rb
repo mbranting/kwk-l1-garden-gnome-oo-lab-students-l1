de88c825740cb6da180e9f8573d46e6a46dc0816
mbranting <mckenna.branting@gmail.com>
2018-06-07 21:49:42 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,4 +1,10 @@
 # Code your instances here
 class GardenGnome
   attr_accessor :age, :gluten_allergy, :personality, :hat_color
-  
+  def
+  @age = age
+  @gluten_allergy = gluten_allergy
+  @personality = personality
+  @hat_color = hat_color
+end 
+def 
