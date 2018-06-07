d3db2bac1d1e05ccc6a82d03d9994507041c7f10
mbranting <mckenna.branting@gmail.com>
2018-06-07 22:02:25 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,14 +1,16 @@
 # Code your instances here
 class GardenGnome
   attr_accessor :name, :age, :gluten_allergy, :personality, :hat_color
-  def initialize(name, age, gluten_allergy, personality, hat_color)
+  def initialize(name, age, gluten_allergy, hat_color,personality = "evil")
   @name= name
   @age = age
   @gluten_allergy = gluten_allergy
   @personality = personality
   @hat_color = hat_color
-  def introduce_self
-    puts "Hello humans, my name is #{name}. 
+    def introduce_self
+      puts "Hello humans, my name is #{name}. I am #{age} years old, and you'll rue the day you crossed me!"
+    end
+  puts introduce_self
   end 
   def gnaw 
     puts "Gnawing on a tree"
@@ -16,4 +18,4 @@ class GardenGnome
   def shout
     puts "GNARLY!!!"
   end
-def 
+  def 
